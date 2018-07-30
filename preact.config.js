3f2769a9805a5899a8732309893fb6ebc9a69358
Leo Tse <leocltse@gmail.com>
2018-07-30 00:01:48 -0400

    Disable service worker plugin.

--- preact.config.js
+++ preact.config.js
@@ -2,20 +2,25 @@ import webpack from 'webpack';
 
 export default function (config, env, helpers) {
 
+	// assets env path
+	env.ASSETS = './assets/';
+
 	// disable code-splitting
 	config.plugins.push(new webpack.optimize.LimitChunkCountPlugin({
 		maxChunks: 1
 	}));
 
-	// assets env path
-	env.ASSETS = './assets/';
-
-	// setup prod public path
 	if (env.production) {
-		config.output.publicPath = '/roadmap';
-	}
 
-	// disable js sourcemaps
-	let { plugin } = helpers.getPluginsByName(config, 'UglifyJsPlugin')[0];
-	plugin.options.sourceMap = false;
+		// app is under /roadmap in prod
+		config.output.publicPath = '/roadmap';
+
+		// disable service worker
+		let { index } = helpers.getPluginsByName(config, 'SWPrecacheWebpackPlugin')[0];
+		config.plugins.splice(index);
+
+		// disable js sourcemaps
+		let { plugin } = helpers.getPluginsByName(config, 'UglifyJsPlugin')[0];
+		plugin.options.sourceMap = false;
+	}
 }
